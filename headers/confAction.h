72c150d655854a230f316220d628f0d5db10bc40
CeliaPiquet <44065646+CeliaPiquet@users.noreply.github.com>
2019-11-15 12:17:54 +0100

    update

--- headers/confAction.h
+++ headers/confAction.h
@@ -10,18 +10,22 @@
 #define confAction_h
 
     ListType* initListType(void);
+
     ListAction* initListAction(int capacity);
     ListAction* getAllActionsFromConf(FILE *fp);
+
     Action* createOneActionFromConf(FILE *fp, int *charReaden);
     Action* initAction(void);
+    
     int completeActionAttribut(Action *action, char *actualLine);
-    void displayAction(Action action);
     int addActionToList(ListAction *listAction, Action *actionToAdd);
     int setActionAttributTabStr(char *attributName, Action *action, char **attributValueTabStr, int sizeOfTabStr);
-    void cleanListAction(ListAction *allActions);
-    void cleanAction(Action *action);
     int setActionAttributInt(char *attributName, Action *action, char *attributValueInt);
+    
+    void cleanAction(Action *action);
+    void cleanListAction(ListAction *allActions);
     void setActionAttributStr(char *attributName, Action *action, char *attributValue);
     void cleanActionAttributVariables(char **attributName, char **charAttribut, char ***stringTabAttribut, char **attributValueInt);
+    void displayAction(Action action);
 
 #endif /* confAction_h */
